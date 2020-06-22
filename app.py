871456c865340d5cf8676a142097a431a61ea97b
Steffen Deubler <steffen.deubler@t-online.de>
2020-06-22 19:26:14 +0200

    Add command line options

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ from pyrt import *
 import jsonpickle
 from celery import chord
 import requests
+import click
 
-def main():
+@click.command()
+@click.option('--samples', default=100, help='Number of samples per pixel')
+@click.option('--width', default=400, help='Width of the image')
+def main(samples, width):
 
     aspect_ratio = 16.0 / 9.0
-    image_width = 400
-    height = int(image_width / aspect_ratio)
-    samples = 100
+    width = 400
+    height = int(width / aspect_ratio)
 
     scene = [Sphere(1, Transform.translation(1, -0.5, -5), Lambert(Vec3(1.0, 0.0, 0.0))),
              Sphere(2, Transform.translation(-2, -1.5, -5), Metal(Vec3(0.9, 0.9, 0.9))),
              Sphere(200, Transform.translation(1, 200.5, -10), Lambert(Vec3(0.0, 1.0, 0.0)))]
 
-    camera = Camera(image_width)
+    camera = Camera(width)
 
     jscene = jsonpickle.encode(scene)
     jcamera = jsonpickle.encode(camera)
@@ -27,7 +30,7 @@ def main():
     print(scanlines)
 
     callback = combine.s()
-    header = [render.s(jscene, jcamera, image_width, aspect_ratio, samples, lines) 
+    header = [render.s(jscene, jcamera, width, aspect_ratio, samples, lines) 
     for lines in scanlines]
 
     result = chord(header)(callback)
